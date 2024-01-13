94dd40cc147d079ee6f038be2187ef4510ca8f7e
Soo-jong Kim <sujjong456@gmail.com>
2024-01-13 23:17:49 +0900

    ?

--- JavaBasic/src/day1/Practice.java
+++ JavaBasic/src/day1/Practice.java
@@ -7,7 +7,7 @@ public class Practice {
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
-		
+		System.out.println("yo");
 int t = sc.nextInt();		
 int a =0 ;
 int b =0 ;
